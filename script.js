f9e71417c838c6822d9f26fa39f97dea72707e4b
charlie <56640971+simplyykin@users.noreply.github.com>
2023-09-09 18:41:55 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function convertScoreACT() {
+    const actScore = parseFloat(document.getElementById('actScore').value);
+    const multiplier = 44;
+
+    if (!isNaN(actScore)) {
+        const satScore = multiplier * actScore;
+       const roundedACTScore = Math.ceil(satScore / 10) * 10;
+        document.getElementById('resultact').textContent = `SAT Score Equivalent: ${roundedACTScore}`;
+    } else {
+        document.getElementById('resultact').textContent = 'Please enter a valid ACT score.';
+    }
+}
+function convertScoreSAT() {
+    const actScore = parseFloat(document.getElementById('actScore1').value);
+    const multiplier = 44.44444444444;
+
+    if (!isNaN(actScore)) {
+        const satScore = actScore / multiplier;
+        document.getElementById('result1').textContent = `ACT Score Equivalent: ${Math.round(satScore)}`;
+    } else {
+        document.getElementById('result1').textContent = 'Please enter a valid SAT score.';
+    }
+}
